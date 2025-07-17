f6cbadf54a19c896e4fa632d2a06efedabb97a93
Nabeel Thurakkal <nthurakkal92@gmail.com>
2025-07-17 02:26:37 +0200

    cleanup

--- src/providers.tsx
+++ src/providers.tsx
@@ -3,7 +3,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
-import { getQueryClient } from "./lib/get-query-client";
+import { getQueryClient } from "@/lib/get-query-client";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => getQueryClient());
